e17aeed96b11140f2a13b4a901c9b8522695390b
Mike <mike.deleon235@gmail.com>
2017-06-21 13:36:19 -0400

    Add files via upload

---
+++ BuildContainers.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+# ======================== Building the Containers ========================= #
+#             For use with the EDCOP project, SealingTech 2017               #
+#                                                                            #
+# Script to build the Docker containers within this directory.               #
+# Be sure to run this script within its native directory!                    #
+#									     #
+# Please check out the offical configuration guide for further information   #
+# github.com/sealingtech/EDCOP-Containers                                    #
+# ========================================================================== #
+sudo docker build -t elasticsearch ./Elasticsearch
+
+sudo docker build -t logstash ./Logstash
+
+sudo docker build -t kibana ./Kibana
+
+sudo docker build -t filebeat ./Filebeat
+
+sudo docker build -t packetbeat ./Packetbeat
+
